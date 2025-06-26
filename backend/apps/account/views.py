a4560b91f94d0e6b1d6ce24f221034016d586e2b
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-06-26 16:12:42 +0800

    Update views.py

--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -1,46 +1,35 @@
-
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, routers, decorators, views, viewsets
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
 
 from .serializers import UserLoginSerializer, UserSerializer
 
 
-class LoginAPIView(APIView):
+class LoginLogoutViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
 
-    def post(self, request):
+    @decorators.action(detail=False, methods=['post'], url_path="login/")
+    def login(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class LogoutAPIView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request):
+    @decorators.action(detail=False, methods=['post'], url_path="login/")
+    def logout(self, request):
         logout(request)
         return Response({"message": "Successfully logged out"}, status=status.HTTP_200_OK)
 
 
-class UserDetailAPIView(generics.RetrieveAPIView):
+class UserDetailModelViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = UserSerializer
 
-    def get_object(self):
-        return self.request.user
 
-
-# 传统视图用于渲染登录页面
-def login_view(request):
-    return render(request, 'rest_framework/login.html')
-
-
-def dashboard_view(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    return render(request, 'dashboard.html')
+router = routers.DefaultRouter()
+# todo: [to be understood] why the router is not working(api doc is not showing)
+router.register(r"loginlogout", LoginLogoutViewSet, basename="login-logout")
+router.register(r"userdetail", UserDetailModelViewSet)
