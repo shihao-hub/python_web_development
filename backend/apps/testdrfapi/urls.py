84cdb64251ca2d47a348f8eb9768f3fae8da9f20
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-06-27 20:56:40 +0800

    Update urls.py

--- backend/apps/testdrfapi/urls.py
+++ backend/apps/testdrfapi/urls.py
@@ -3,11 +3,11 @@ from rest_framework import routers
 
 from .views import (
     UserModelViewSet, GroupModelViewSet, TestViewSet,
-    CourseList,
+    CourseList, CourseDetail,
+    GCourseDetail,
     course_list, course_detail,
 )
 
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserModelViewSet)
 router.register(r'groups', GroupModelViewSet)
@@ -15,7 +15,14 @@ router.register(r'test', TestViewSet, basename='test')
 
 urlpatterns = [
     path(r"", include(router.urls)),
-    path(r"fbv/course_list/", course_list, name="fbv-course_list"),
-    path(r"fbv/detail/<int:pk>", course_detail, name="fbv-course_detail"),
-    path(r"cbv/list/", CourseList.as_view(), name='cbv-list')
+
+    # summary: 本人目前推荐使用的是：ModelViewSet, ViewSet, decorators.action, APIView 等
+    path(r"fbv/list/", course_list, name="fbv-list"),
+    path(r"fbv/detail/<int:pk>/", course_detail, name="fbv-detail"),
+
+    path(r"cbv/list/", CourseList.as_view(), name='cbv-list'),
+    path(r"cbv/detail/<int:pk>/", CourseDetail.as_view(), name='cbv-detail'),
+
+    # path(r"gcbv/list/", GCourseList.as_view(), name='gcbv-list'),
+    path(r"gcbv/detail/<int:pk>/", GCourseDetail.as_view(), name='gcbv-detail'),
 ]
