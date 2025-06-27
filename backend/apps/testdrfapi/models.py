9b5ed29dbb30d4bf1749da5fabad322070bbc107
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-06-27 20:54:54 +0800

    Update models.py

--- backend/apps/testdrfapi/models.py
+++ backend/apps/testdrfapi/models.py
@@ -3,14 +3,15 @@ from django.conf import settings
 
 
 class Course(models.Model):
-    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, help_text='课程讲师',
-                                verbose_name='讲师')
+    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='讲师', on_delete=models.CASCADE,
+                                help_text='课程讲师')
 
-    name = models.CharField(max_length=255, unique=True, help_text='课程名称', verbose_name='名称')
-    introduction = models.TextField(help_text='课程介绍', verbose_name='介绍')
+    name = models.CharField(verbose_name='名称', max_length=255, unique=True, help_text='课程名称')
+    # TextField 似乎有默认值，空字符串
+    introduction = models.TextField(verbose_name='介绍', help_text='课程介绍')
     price = models.DecimalField(max_digits=6, decimal_places=2, help_text='课程价格')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
-    update_at = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+    created_at = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name='更新时间', auto_now=True)
 
     class Meta:
         verbose_name = "课程信息"
