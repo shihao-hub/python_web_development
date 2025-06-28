20b9bed71d8a6d38346769afc66a18b3a55e0ab9
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-06-28 18:22:42 +0800

    Update urls.py

--- backend/apps/testdrfapi/urls.py
+++ backend/apps/testdrfapi/urls.py
@@ -2,19 +2,24 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .views import (
-    UserModelViewSet, GroupModelViewSet, TestViewSet,
+    UserModelViewSet, GroupModelViewSet,
+    TestViewSet, CourseViewSet,
+    course_list, course_detail,
     CourseList, CourseDetail,
     GCourseDetail,
-    course_list, course_detail,
+    jupyternotebooks_test,
 )
 
 router = routers.DefaultRouter()
 router.register(r'users', UserModelViewSet)
 router.register(r'groups', GroupModelViewSet)
 router.register(r'test', TestViewSet, basename='test')
+router.register(r'viewset', CourseViewSet, basename='viewset')
 
 urlpatterns = [
     path(r"", include(router.urls)),
+
+    path(r"jupyternotebooks/test/", jupyternotebooks_test, name="jupyternotebooks_test"),
 
     # summary: 本人目前推荐使用的是：ModelViewSet, ViewSet, decorators.action, APIView 等
     path(r"fbv/list/", course_list, name="fbv-list"),
