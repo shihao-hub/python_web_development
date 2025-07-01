e996b03189e0266475f404f6c66e2826b97459c6
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-01 09:11:28 +0800

    Update urls.py

--- backend/apps/powernetwork/urls.py
+++ backend/apps/powernetwork/urls.py
@@ -7,5 +7,6 @@ router = routers.DefaultRouter()
 router.register(r'bus', BusModelViewSet, basename='bus')
 
 urlpatterns = [
+    # todo: 这个 name 被设置成了 api-root，这没有命名空间的话，不同 app 的 url 是否会混乱呢？
     path("", include(router.urls))
 ]
