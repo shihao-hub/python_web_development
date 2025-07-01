aede01876f7ef532db069315ba17e746fe6fb124
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-01 09:16:17 +0800

    Update apps.py

--- backend/apps/testdrfapi/apps.py
+++ backend/apps/testdrfapi/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class TempConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.testdrfapi'
-    verbose_name = "apps-testdrfapi"
+    verbose_name = "测试 - drf api"
