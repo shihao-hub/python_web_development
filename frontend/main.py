9a764250fdbc41068f7650bcf9851cfa1d5b049d
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-03 21:54:25 +0800

    Update main.py

--- frontend/main.py
+++ frontend/main.py
@@ -1,9 +1,27 @@
-from nicegui import ui
+from nicegui import ui, native
+
+from nicegui_toolkit.layout_tool import inject_layout_tool
 
 from frontend.settings import TITLE, HOST, PORT, SECRET_KEY
 
 from pages import home, login  # todo: 确定一下，pages 代码执行怎么合理的执行，单纯导入似乎不合理
 
-if __name__ == '__main__':
+
+# todo: 它导致 button 无法点击了啊？难受！能解决吗？
+# inject_layout_tool(ide="pycharm", language_locale="zh")
+
+
+# temp
+@ui.page("/")
+def index_page():
+    ui.navigate.to("/home")
+
+
+def run(port: int = PORT, native_option: bool = False):
     # 实际上启动了 FastAPI 服务器
-    ui.run(title=TITLE, host=HOST, port=PORT, reload=False, show=False, favicon="🚀", storage_secret=SECRET_KEY)
+    ui.run(title=TITLE, host=HOST, port=port, reload=False, show=False, favicon="🚀", storage_secret=SECRET_KEY,
+           native=native_option)
+
+
+if __name__ == '__main__':
+    run()
