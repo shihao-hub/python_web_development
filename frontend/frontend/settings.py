71bfb715e3dae68bf4b56435e0a1d9647841bbcb
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-03 21:53:07 +0800

    Update settings.py

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -1,14 +1,16 @@
 import configparser
 from pathlib import Path
 
+# todo: 这个打包之后就存在问题了呀！
 BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_DIR = BASE_DIR.parent
 
 config = configparser.ConfigParser()
+# fixme: 这个路径容易找不到！
 config.read(ROOT_DIR / "conf" / "config.ini")
 
 TITLE = "柔性配电评估系统"
 HOST = "localhost"
-PORT = config.getint("settings", "frontend_port")
+PORT = 12000  # config.getint("settings", "frontend_port")
 
 SECRET_KEY = 'django-insecure-ok))2mb#mev0$i$9i9-c9130*r26iveph$$=-927l(uwp9i4(k'
