5fb96271000bdcbedc78014f58e943815576cd83
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-03 21:52:21 +0800

    Update temporary.py

--- frontend/examples/temporary.py
+++ frontend/examples/temporary.py
@@ -1,17 +1,25 @@
 from loguru import logger
 
+import math
+from datetime import datetime
 from nicegui import ui
-from uuid import uuid4
+
+line_plot = ui.line_plot(n=2, limit=20, figsize=(3, 2), update_every=5) \
+    .with_legend(['sin', 'cos'], loc='upper center', ncol=2)
 
 
-@ui.page('/private_page')
-async def private_page():
-    ui.label(f'private page with ID {uuid4()}')
+def update_line_plot() -> None:
+    now = datetime.now()
+    x = now.timestamp()
+    y1 = math.sin(x)
+    y2 = math.cos(x)
+    line_plot.push([now], [[y1], [y2]])
 
 
-ui.label(f'shared auto-index page with ID {uuid4()}')
-ui.link('private page', private_page)
+line_updates = ui.timer(0.1, update_line_plot, active=False)
+line_checkbox = ui.checkbox('active').bind_value(line_updates, 'active')
 
 # ui.run()
 
-ui.run(host="localhost", port=14000, reload=False, show=False, favicon="🚀", storage_secret="123")
+
+ui.run(host="localhost", port=14000, reload=False, show=False, favicon="🚀", storage_secret="123", on_air=True)
