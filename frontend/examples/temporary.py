50e972db22cd2c3b4309ca8db81205887776ca40
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-04 17:35:50 +0800

    Update temporary.py

--- frontend/examples/temporary.py
+++ frontend/examples/temporary.py
@@ -1,24 +1,25 @@
 import os
 from typing import Optional
 
-from dotenv import load_dotenv
-from loguru import logger
 
 from nicegui import ui
 
-with ui.timeline(side='right'):
-    ui.timeline_entry('Rodja and Falko start working on NiceGUI.',
-                      title='Initial commit',
-                      subtitle='May 07, 2021')
-    ui.timeline_entry('The first PyPI package is released.',
-                      title='Release of 0.1',
-                      subtitle='May 14, 2021')
-    ui.timeline_entry('Large parts are rewritten to remove JustPy '
-                      'and to upgrade to Vue 3 and Quasar 2.',
-                      title='Release of 1.0',
-                      subtitle='December 15, 2022',
-                      icon='rocket')
+ui.aggrid({
+    'columnDefs': [
+        {'headerName': 'Name', 'field': 'name'},
+        {'headerName': 'Age', 'field': 'age', 'cellClassRules': {
+            'bg-red-300': 'x < 21',
+            'bg-green-300': 'x >= 21',
+        }},
+    ],
+    'rowData': [
+        {'name': 'Alice', 'age': 18},
+        {'name': 'Bob', 'age': 21},
+        {'name': 'Carol', 'age': 42},
+    ],
+})
 
 # ui.run()
+
 
 ui.run(host="localhost", port=14000, reload=False, show=False, favicon="🚀", storage_secret="123")
