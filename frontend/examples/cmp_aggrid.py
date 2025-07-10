af24beea0a39b6f0ba797ef2caa36509cfb5114e
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-10 15:48:52 +0800

    Update cmp_aggrid.py

--- frontend/examples/cmp_aggrid.py
+++ frontend/examples/cmp_aggrid.py
@@ -7,31 +7,31 @@
 
 from nicegui import ui
 
-grid = ui.aggrid({
-    'defaultColDef': {'flex': 1},
-    'columnDefs': [
-        {'headerName': 'Name', 'field': 'name'},
-        {'headerName': 'Age', 'field': 'age'},
-        {'headerName': 'Parent', 'field': 'parent', 'hide': True},
-    ],
-    'rowData': [
-        {'name': 'Alice', 'age': 18, 'parent': 'David'},
-        {'name': 'Bob', 'age': 21, 'parent': 'Eve'},
-        {'name': 'Carol', 'age': 42, 'parent': 'Frank'},
-    ],
-    'rowSelection': 'multiple',
-}).classes('max-h-40')
-
-
-def update():
-    grid.options['rowData'][0]['age'] += 1
-    grid.update()
-
-
-# todo: aggrid
-
-ui.button('Update', on_click=update)
-ui.button('Select all', on_click=lambda: grid.run_grid_method('selectAll'))
-ui.button('Show parent', on_click=lambda: grid.run_grid_method('setColumnsVisible', ['parent'], True))
+# grid = ui.aggrid({
+#     'defaultColDef': {'flex': 1},
+#     'columnDefs': [
+#         {'headerName': 'Name', 'field': 'name'},
+#         {'headerName': 'Age', 'field': 'age'},
+#         {'headerName': 'Parent', 'field': 'parent', 'hide': True},
+#     ],
+#     'rowData': [
+#         {'name': 'Alice', 'age': 18, 'parent': 'David'},
+#         {'name': 'Bob', 'age': 21, 'parent': 'Eve'},
+#         {'name': 'Carol', 'age': 42, 'parent': 'Frank'},
+#     ],
+#     'rowSelection': 'multiple',
+# }).classes('max-h-40')
+#
+#
+# def update():
+#     grid.options['rowData'][0]['age'] += 1
+#     grid.update()
+#
+#
+# # todo: aggrid
+#
+# ui.button('Update', on_click=update)
+# ui.button('Select all', on_click=lambda: grid.run_grid_method('selectAll'))
+# ui.button('Show parent', on_click=lambda: grid.run_grid_method('setColumnsVisible', ['parent'], True))
 
 ui.run(host="localhost", port=14000, reload=False, show=False, favicon="🚀", storage_secret="123")
