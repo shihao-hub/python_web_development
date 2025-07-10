828c6a937a8ef0dcf0d429512d8f15aa046bdca3
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-10 15:49:48 +0800

    Update temporary.py

--- frontend/examples/temporary.py
+++ frontend/examples/temporary.py
@@ -1,17 +1,6 @@
-from nicegui import Tailwind, ui
-
-ui.label('Label A').tailwind.font_weight('extrabold').text_color('blue-600').background_color('orange-200')
-ui.label('Label B').tailwind('drop-shadow', 'font-bold', 'text-green-600')
-
-red_style = Tailwind().text_color('red-600').font_weight('bold')
-label_c = ui.label('Label C')
-red_style.apply(label_c)
-ui.label('Label D').tailwind(red_style)
-
-ui.button().tailwind()
+from nicegui import ui
 
 
-# ui.run()
 
 
 ui.run(host="localhost", port=14000, reload=False, show=False, favicon="🚀", storage_secret="123")
