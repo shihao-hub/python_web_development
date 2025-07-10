f674994fd31ca103620f29ae4ba5cc3ee5302327
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-10 15:49:02 +0800

    Create cmp_header_footer_drawer.py

---
+++ frontend/examples/cmp_header_footer_drawer.py
@@ -0,0 +1,23 @@
+from loguru import logger
+
+from nicegui import ui
+
+@ui.page('/page_layout')
+def page_layout():
+    ui.label('CONTENT')
+    [ui.label(f'Line {i}') for i in range(100)]
+    with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
+        ui.label('HEADER')
+        ui.button(on_click=lambda: right_drawer.toggle(), icon='menu').props('flat color=white')
+    with ui.left_drawer(top_corner=True, bottom_corner=True).style('background-color: #d7e3f4'):
+        ui.label('LEFT DRAWER')
+    with ui.right_drawer(fixed=False).style('background-color: #ebf1fa').props('bordered') as right_drawer:
+        ui.label('RIGHT DRAWER')
+    with ui.footer().style('background-color: #3874c8'):
+        ui.label('FOOTER')
+
+ui.link('show page with fancy layout', page_layout)
+
+# ui.run()
+
+ui.run(host="localhost", port=14000, reload=False, show=False, favicon="🚀", storage_secret="123")
