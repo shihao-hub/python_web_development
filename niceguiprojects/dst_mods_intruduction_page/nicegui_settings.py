670cd9393827463e56ca486e8837d0f60087d76f
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-14 21:02:29 +0800

    Update and rename settings.py to nicegui_settings.py

--- niceguiprojects/dst_mods_intruduction_page/nicegui_settings.py
+++ niceguiprojects/dst_mods_intruduction_page/nicegui_settings.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
 
-class settings:  # noqa: Class names should use CapWords convention
+class nicegui_settings:  # noqa: Class names should use CapWords convention
     """临时充当 settings.py 使用的类"""
     UPGRADING = False
 
 
-UPGRADING = settings.UPGRADING
-
 DEBUG = True
+
+UPGRADING = nicegui_settings.UPGRADING
 
 # todo: 尽量多使用 Path，少用 os.path
 ROOT_DIR = Path(__file__).resolve().parent  # 项目根目录
@@ -17,3 +17,8 @@ STATIC_DIR = ROOT_DIR / "static"
 UPLOADED_DIR = ROOT_DIR / "storage" / "uploads"
 
 CACHE_TTL = 60 * 60 * 12 if not DEBUG else 2  # 缓存的时间
+
+TITLE = "心悦卿兮的饥荒模组合集"
+FAVICON = "🌿"
+HOST = "localhost"
+PORT = 15001
