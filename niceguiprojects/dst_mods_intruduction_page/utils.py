ce691cf8bfbbe60c5622ca66a2ae2367b66f3070
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-14 21:01:10 +0800

    Update utils.py

--- niceguiprojects/dst_mods_intruduction_page/utils.py
+++ niceguiprojects/dst_mods_intruduction_page/utils.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import cachetools
 from loguru import logger
 
-from settings import ROOT_DIR, STATIC_DIR
+from nicegui_settings import ROOT_DIR, STATIC_DIR
 
 
 class FileUploadError(Exception):
@@ -18,11 +18,15 @@ class FileUploadManager:
         pass
     """
 
+    STORAGE_PATH = ROOT_DIR / "storage" / "uploads"
+    TEMP_STORAGE_PATH = ROOT_DIR / "storage" / "temp_uploads"
+
     def __init__(self):
-        self._storage_path = ROOT_DIR / "storage" / "uploads"
-        self._temp_storage_path = ROOT_DIR / "storage" / "temp_uploads"
+        self._storage_path = self.STORAGE_PATH
+        self._temp_storage_path = self.TEMP_STORAGE_PATH
         self._saved_file_size = 0
         self._max_file_size = 1024 * 1024 * 1024 * 10  # 10GB
+        self._max_temp_file_size = 1024 * 1024 * 1024 * 1  # 1GB
 
     def _check_max_file_size(self, new_file_size: int):
         if self._saved_file_size + new_file_size > self._max_file_size:
@@ -30,17 +34,26 @@ class FileUploadManager:
 
     def _check_temp_dir_state(self):
         """检查临时目录的状态，并考虑是否要启动清除临时目录程序"""
-        # 我有点无语，ui.upload 这么难用吗？
+        # 我有点无语，ui.upload 这么难用吗？不科学啊。
+
+        # 暂时直接检查文件数量，1GB / 10MB = 100
+        files_number = 0
+        if files_number > 100:
+            # 【亮点】启动清除临时目录程序，删除文件应该使用 io 操作，所以可以使用 python 线程/进程 或者 nicegui 提供的 await run.io_bound？
+            pass
 
     def save(self,
              filecontent: bytes,
              file_extension: str,
              filename: str = None,
              is_temp: bool = False,  # 是否是临时文件
+             ret_relative_path: bool = False
              ) -> Path:  # 返回文件路径
         """存储文件"""
         if filename is None:
             filename = str(uuid.uuid4()) + ("" if file_extension is None else file_extension)
+
+        # todo: file_extension 是否需要兼容？用户如果输入文件名，是不是可以帮用户解析？
 
         storage_path = self._storage_path
 
@@ -54,6 +67,8 @@ class FileUploadManager:
         self._saved_file_size += view.nbytes
         filepath.write_bytes(filecontent)
         logger.info("存储" + ("临时" if is_temp else "") + "文件成功，文件名：{}", filename)
+        if ret_relative_path:
+            return Path(filename)
         return filepath
 
     def delete(self, filename: str,
