3edbdef4be7e173addfd004e54c51971ec7aa1f7
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-14 21:00:54 +0800

    Update settings.py

--- niceguiprojects/dst_mods_intruduction_page/djangoorm/djangoorm/settings.py
+++ niceguiprojects/dst_mods_intruduction_page/djangoorm/djangoorm/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # sys.path.insert(0, os.path.join(BASE_DIR, ".."))
 
-SECRET_KEY = "lyv+won7%7!=ra!nc160o-x1yz+m%n1jxm)wtw_y1r3%shh@-%"
+SECRET_KEY = "lyv+won7%7!=ra!nc160o-x1yz+m%n1jxm)wtw_y1r3%shh@-%X"
 
 DEBUG = True
 
@@ -17,6 +17,6 @@ INSTALLED_APPS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "test_db.sqlite3") if DEBUG else os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
