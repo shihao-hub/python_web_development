640845f51e1f34aba487de2f5210b6d7d7a01724
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-15 15:37:53 +0800

    Update __init__.py

--- niceguiprojects/dst_mods_intruduction_page/djangoorm/__init__.py
+++ niceguiprojects/dst_mods_intruduction_page/djangoorm/__init__.py
@@ -1,17 +1,32 @@
 __all__ = [
-    "load_djangoorm"
+    "load_djangoorm",
+    "settings",  # todo: 确定一下这是 django.conf 的 settings，有很多多余的东西，单纯使用 django orm 是否需要呢？
+    "nicegui_settings",
+    "models",
+    "utils"
 ]
 
-import os
 
-# 必须在导入 django 内容前设置 DJANGO_SETTINGS_MODULE
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoorm.djangoorm.settings")
+def _execute_before_anything():
+    import os
 
-from django.apps import apps  # noqa
-from django.conf import settings  # noqa
+    # 必须在导入 django 内容前设置 DJANGO_SETTINGS_MODULE
+    # 注意，按照我目前的理解，启动整个项目的入口文件与 djangoorm 目录应该是同级的才行
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoorm.djangoorm.settings")
 
-# 把所有的 app 加载到 Django 里
-apps.populate(settings.INSTALLED_APPS)
+    from django.apps import apps
+    from django.conf import settings
+
+    # 把所有的 app 加载到 Django 里
+    apps.populate(settings.INSTALLED_APPS)
+
+
+_execute_before_anything()
+
+from django.conf import settings
+
+from .app import models, utils  # 注意，此处需要用 .app 的方式才能找到对于 Python Package
+from .djangoorm.settings import nicegui_settings  # 注意，django.conf 的 settings 无法将 settings.py 中的 class 解析！
 
 
 def load_djangoorm(migrate=True):
