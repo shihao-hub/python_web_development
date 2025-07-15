78f759231deba2d1ebef523e3b04d6379637bb8e
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-15 20:39:52 +0800

    Update urls.py

--- backend/apps/testdjangoapi/urls.py
+++ backend/apps/testdjangoapi/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from .views import TestTemplateView
 
 urlpatterns = [
+    # todo: 熟悉 drf 和 django 的所有视图类
     path("test/", TestTemplateView.as_view(), name="test"),
 ]
