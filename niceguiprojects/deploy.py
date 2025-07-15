42a5130f4bdaddbaac635e26e098b8a09d031fed
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-15 15:37:04 +0800

    Create deploy.py

---
+++ niceguiprojects/deploy.py
@@ -0,0 +1,14 @@
+"""
+## 需求背景
+
+niceguiprojects 目录下的子目录均是一个独立项目，为了方便才将它们放在一起。
+
+为此，出现了如下需求：
+
+- 开发完毕后，将指定目录复制到某个目录下（覆盖的方式复制过去，当然保留那个目录下的 .git，为了后续将代码提交到代码仓）
+- git_push.py
+- DEBUG -> true
+- Docker：https://www.bilibili.com/video/BV1THKyzBER6
+
+
+"""
